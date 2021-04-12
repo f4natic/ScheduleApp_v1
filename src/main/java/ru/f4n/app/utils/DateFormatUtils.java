33d331341822b94fc0be0e411e450e5fe21cad33
f4natic <infr4rot@gmail.com>
2021-04-12 17:24:08 +0700

    Add select to purchased classes and date formater class

--- src/main/java/ru/f4n/app/utils/DateFormatUtils.java
+++ src/main/java/ru/f4n/app/utils/DateFormatUtils.java
@@ -1,4 +1,31 @@
 package ru.f4n.app.utils;
 
-public class DateFormatUtils {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class DateFormatUtils {
+    private static String dateFormatPattern = "yyyy-MM-dd hh:mm";
+    private static SimpleDateFormat dateFormat;
+
+    public static Date getDate(String date) {
+        dateFormat = new SimpleDateFormat();
+        dateFormat.applyPattern(dateFormatPattern);
+        Date parseDate = null;
+        try {
+             parseDate = dateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.print("Неверный формат даты");
+            return new Date();
+        }
+        return parseDate;
+    }
+
+    public static String getStringDate(Date date) {
+        dateFormat = new SimpleDateFormat();
+        dateFormat.applyPattern(dateFormatPattern);
+        dateFormat.format(date);
+        return dateFormat.format(date);
+    }
 }
